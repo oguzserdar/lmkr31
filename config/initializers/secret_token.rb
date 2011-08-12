8823ff2baa724b7efa989a4af4968c02c8f9e4b1
Oğuz Demirkapı <demirkapi@gmail.com>
2011-08-11 20:22:12 -0400

    rails 3.1.0.rc5 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lmk::Application.config.secret_token = '92bf10ac19cec645c140754a9aafc4069b9a0137d7dff9d71066599028cdf50a9526f68b20257e5059ef3b021d2b6a08efd4244a9d70c15e93a1b0781a083a58'
